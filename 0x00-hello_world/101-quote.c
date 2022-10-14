f69de65c3bf5a657eee6f6b52c3a42413f147519
Omotolanny <omotolanionifade@gmail.com>
2022-10-14 15:53:25 -0700

    hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 /**
  *  * main - Prints out the last part of a quote in the standard erro
@@ -7,7 +7,7 @@ r.
  */
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 20
+	write(2, "and that piece of art is useful\" - Dora Korpar, 20
 15-10-19\n", 59);
-        return (1);
+	return (1);
 }
